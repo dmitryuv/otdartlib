fae0b2a36154b7d4971c5cd252e8a75fcfe53f19
Dmitry Uvarov <uvarov.dl@gmail.com>
2016-12-13 00:52:59 +0300

    immutable Position

--- lib/src/atext_changeset/position.dart
+++ lib/src/atext_changeset/position.dart
@@ -1,10 +1,10 @@
 part of otdartlib.atext_changeset;
 
-class Position implements Clonable {
-  int ch = 0;
-  int line = 0;
+class Position {
+  final int ch;
+  final int line;
   
-  Position([this.ch = 0, this.line = 0]);
+  const Position([this.ch = 0, this.line = 0]);
   
   /**
    * Returns true if current position is before other position
@@ -14,21 +14,15 @@ class Position implements Clonable {
   int get hashCode => hash2(ch, line);
   
   bool operator==(other) => other is Position && line == other.line && ch == other.ch;
-  
-  /**
-   * Clone this position object into new one
-   */
-  @override
-  Position clone() => new Position(ch, line);
- 
+
   /**
    * Adding lines does not alter char position in new line
    */
-  void add(int chars, int lines) {
+  Position add(int chars, int lines) {
     if(lines != 0) {
-      line += lines;
+      return new Position(ch, line + lines);
     } else {
-      ch += chars;
+      return new Position(ch + chars, line);
     }
   }
 
@@ -36,22 +30,23 @@ class Position implements Clonable {
    * Unlike adding, advance uses changeset logic to advance between lines
    * i.e. moving to next line resets char position
    */
-  void advance(int chars, int lines) {
-    add(chars, lines);
+  Position advance(int chars, int lines) {
+    var p = add(chars, lines);
     if(lines != 0) {
-      ch = 0;
+      return new Position(0, p.line);
     }
+    return p;
   }
   
   /**
    * Subtracts component chars or lines from position. Char position on this line should not be affected
    * if we're removing other linesэ
    */
-  void subtract(int chars, int lines) {
+  Position subtract(int chars, int lines) {
     if(lines != 0) {
-      line -= lines;
+      return new Position(ch, line - lines);
     } else {
-      ch -= chars;
+      return new Position(ch - chars, line);
     }
   }
 
@@ -65,7 +60,7 @@ class Position implements Clonable {
       throw new Exception('side should be \'left\' or \'right\'');
     }
 
-    var res = this.clone();
+    var res = this;
     // iteration cursor
     var c = new Position();
 
@@ -74,31 +69,30 @@ class Position implements Clonable {
         if(c.before(res) || (c == res && side == 'right')) {
           // insert can split current line
           if(op.lines > 0 && res.line == c.line) {
-            res..subtract(c.ch, 0)
-              ..add(0, op.lines);
+            res = res.subtract(c.ch, 0)
+                      .add(0, op.lines);
           } else {
-            res.add(op.chars, op.lines);
+            res = res.add(op.chars, op.lines);
           }
         }
         // advance cursor
-        c.advance(op.chars, op.lines);
+        c = c.advance(op.chars, op.lines);
       } else if(op.isRemove) {
-        var inRange = c.before(res) && res.before(c.clone()..advance(op.chars, op.lines));
+        var inRange = c.before(res) && res.before(c.advance(op.chars, op.lines));
         if(c.before(res) && !inRange) {
           // remove muiltiline range can join cursor row with position row, if they end up on the same row
           if (op.lines > 0 && (c.line + op.lines) == res.line) {
-            res.add(c.ch, 0);
-            res.line = c.line;
+            res = new Position(res.ch + c.ch, c.line);
           } else {
-            res.subtract(op.chars, op.lines);
+            res = res.subtract(op.chars, op.lines);
           }
         } else if (inRange) {
           // we're collapsing range where current position is
-          res = c.clone();
+          res = c;
         }
       } else {
         // KEEP, just advance our position
-        c.advance(op.chars, op.lines);
+        c = c.advance(op.chars, op.lines);
       }
 
       // iterator break - if we passed calculated position, we can stop iterating over ops
--- test/atext_changeset/position-test.dart
+++ test/atext_changeset/position-test.dart
@@ -7,17 +7,17 @@ void position_test() {
     });
   
     test('add', () {
-      expect(new Position(1, 1)..add(1, 0), equals(new Position(2, 1)));
-      expect(new Position(1, 1)..add(1, 1), equals(new Position(1, 2)));
+      expect(new Position(1, 1).add(1, 0), equals(new Position(2, 1)));
+      expect(new Position(1, 1).add(1, 1), equals(new Position(1, 2)));
     });
   
     test('advance', () {
-      expect(new Position(1, 1)..advance(1, 1), equals(new Position(0, 2)));
+      expect(new Position(1, 1).advance(1, 1), equals(new Position(0, 2)));
     });
   
     test('subtract', () {
-      expect(new Position(5, 5)..subtract(1, 0), equals(new Position(4, 5)));
-      expect(new Position(5, 5)..subtract(1, 1), equals(new Position(5, 4)));
+      expect(new Position(5, 5).subtract(1, 0), equals(new Position(4, 5)));
+      expect(new Position(5, 5).subtract(1, 1), equals(new Position(5, 4)));
     });
   
     test('compare', () {
